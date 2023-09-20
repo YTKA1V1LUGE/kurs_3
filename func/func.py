5edfa1719f6c131a520d9b2ddd83ba2d6c5bb0bc
YTKA <stasfyai73364@gmail.com>
2023-09-21 01:49:24 +0700

    Если вернешься к работе correct_format то доделай через цикл в мейн

--- func/func.py
+++ func/func.py
@@ -17,7 +17,13 @@ class account_transactions:
         self.date_operation = []
         self.correct_date_operation = []
         self.dates = []
-
+        self.from_operation = []
+        self.id_operation = []
+        self.state_operation = []
+        self.operation_amount = []
+        self.name_operation = []
+        self.description_operation = []
+        self.to_operation = []
     def __repr__(self):
         return "Класс для выводасообщения об операциях"
 
@@ -58,22 +64,60 @@ class account_transactions:
             self.correct_date_operation.append(days.split("T")[0])
         '''for i in self.correct_date_operation:
             date_operation = datetime.datetime.strptime(i, '%Y-%m-%d').date()''' # я не знаю как и зачем
-        return self.correct_date_operation
+        return self.correct_date_operation, self.full_date_operation
 
+    """
     def correct_format(self):
-        full_date_operation = self.correct_date()
+        self.full_date_operation = self.correct_date()[1]
         for operation in self.load_operation_json:
-            #   if "date" in operation:
-            if operation["date"] == self.full_date_operation[0]:
-                id_operation = operation["id"]  # id
-                state_operation = operation["state"]  # Статус
-                operation_amount = (operation["operationAmount"])["amount"]  # Сумма перевода
-                name_operation = (operation["operationAmount"])["currency"]["name"]  # Валюта
-                description_operation = operation["description"]  # описание перевода
-                from_operation = operation["from"]
-                to_operation = operation["to"]
-                return id_operation, state_operation, operation_amount, name_operation, description_operation, from_operation, to_operation
+            if "date" in operation:
+                if operation["date"] == self.full_date_operation[1]:
+                    self.id_operation = operation["id"]  # id
+                    state_operation = operation["state"]  # Статус
+                    operation_amount = (operation["operationAmount"])["amount"]  # Сумма перевода
+                    name_operation = (operation["operationAmount"])["currency"]["name"]  # Валюта
+                    description_operation = operation["description"]  # описание перевода
+                    if "from" in operation:
+                        self.from_operation = operation["from"]
+                    else:
+                        pass
+                    self.to_operation.append(operation["to"])
+                    return self.id_operation, state_operation, operation_amount, name_operation, description_operation, self.from_operation, self.to_operation
+    """
+    def correct_format(self, date_for_calculated):
+        self.full_date_operation = self.correct_date()[1]#[date_for_calculated]
+        #print(self.full_date_operation)
+        for operation in self.load_operation_json:
+            #if "date" in operation:
+                if operation["date"] == self.full_date_operation[date_for_calculated]:
 
+                    print(self.full_date_operation)
+                    self.id_operation = operation["id"]  # id
+                    self.state_operation.append(operation["state"])  # Статус
+                    self.operation_amount.append(operation["operationAmount"]["amount"])  # Сумма перевода
+                    self.name_operation.append(operation["operationAmount"]["currency"]["name"])  # Валюта
+                    self.description_operation.append(operation["description"])  # описание перевода
+                    if "from" in operation:
+                        self.from_operation.append(operation["from"])
+                    else:
+                        pass
+                    self.to_operation.append(operation["to"])
+                    return self.id_operation, self.state_operation, self.operation_amount, self.name_operation, self.description_operation, self.from_operation, self.to_operation
 
 a = account_transactions()
-print(a.correct_format())
+b = "2019-08-26T10:50:58.294041"
+#print(a.correct_format(1))
+
+
+i = 0
+qwerty = []
+while i != len(a.correct_date()[1]):
+    #qwerty.append(a.correct_format(i))
+    qwerty = a.correct_format(i)
+    #print(i)
+    i += 1
+    print(f"{qwerty}")
+
+
+
+print(a.correct_date()[0])
