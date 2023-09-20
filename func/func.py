25ef0dc37e3878512f736a9196c7faf5ec06ca01
YTKA <stasfyai73364@gmail.com>
2023-09-21 04:45:35 +0700

    что я делаю

--- func/func.py
+++ func/func.py
@@ -13,17 +13,19 @@ def load_operation_json():
 class account_transactions:
     def __init__(self):
         self.load_operation_json = load_operation_json()
-        self.full_date_operation = []
+        self.full_date_operation = None
         self.date_operation = []
         self.correct_date_operation = []
         self.dates = []
-        self.from_operation = []
+        self.from_operation = None
         self.id_operation = []
         self.state_operation = []
         self.operation_amount = []
         self.name_operation = []
         self.description_operation = []
         self.to_operation = []
+        self.date_for_calculation = None # переменная чтобы не вызывать посстояно correct_date
+        self.opera = -1
     def __repr__(self):
         return "Класс для выводасообщения об операциях"
 
@@ -84,14 +86,18 @@ class account_transactions:
                     self.to_operation.append(operation["to"])
                     return self.id_operation, state_operation, operation_amount, name_operation, description_operation, self.from_operation, self.to_operation
     """
-    def correct_format(self, date_for_calculated):
-        self.full_date_operation = self.correct_date()[1]#[date_for_calculated]
-        #print(self.full_date_operation)
-        for operation in self.load_operation_json:
+    """
+    def correct_format(self):
+        self.date_for_calculation = self.correct_date() # хрень чтобы получать даты
+        self.full_date_operation = self.date_for_calculation[1] # хрень чтобы получать полную дату
+        for operation in self.load_operation_json: #цикл в джисон
+            opera = 0   # счетчик для списка
+            while opera != len(self.full_date_operation)-1: #ААААААААА
+                opera += 1
             #if "date" in operation:
-                if operation["date"] == self.full_date_operation[date_for_calculated]:
-
-                    print(self.full_date_operation)
+                if operation["date"] == self.full_date_operation[opera]:
+                    opera += 1
+                    #print(self.full_date_operation)
                     self.id_operation = operation["id"]  # id
                     self.state_operation.append(operation["state"])  # Статус
                     self.operation_amount.append(operation["operationAmount"]["amount"])  # Сумма перевода
@@ -103,21 +109,60 @@ class account_transactions:
                         pass
                     self.to_operation.append(operation["to"])
                     return self.id_operation, self.state_operation, self.operation_amount, self.name_operation, self.description_operation, self.from_operation, self.to_operation
+    """
+    """
+    def correct_format(self, opera):
+        self.date_for_calculation = self.correct_date() # хрень чтобы получать даты
+        #self.full_date_operation = self.date_for_calculation[1][4] # ->
+     #   Как получается так что если поменять цифру во второй квадратной скобке, например на 0 он выводит лишь одинаковые числа
+        for operation in self.load_operation_json:  # цикл в джисон
+                self.full_date_operation = self.date_for_calculation[1]
+                self.full_date_operation = self.full_date_operation[opera]
+                #print(self.full_date_operations)
+                #print(opera)
+                if operation["date"] == self.full_date_operation:
+                    self.id_operation = operation["id"]  # id
+                    self.state_operation = operation["state"]  # Статус
+                    self.operation_amount = operation["operationAmount"]["amount"]  # Сумма перевода
+                    self.name_operation = operation["operationAmount"]["currency"]["name"]  # Валюта
+                    self.description_operation = operation["description"]  # описание перевода
+                    if "from" in operation:
+                        self.from_operation = operation["from"]
+                    else:
+                        pass
+                    self.to_operation = operation["to"]
+                    return self.date_for_calculation[0][opera], self.id_operation, self.state_operation, self.operation_amount, self.name_operation, self.description_operation, self.from_operation, self.to_operation
+    """
+    def correct_format(self):
+        self.date_for_calculation = self.correct_date()  # хрень чтобы получать даты +
+        self.full_date_operation = self.date_for_calculation[1]  # +
+        dates = []
+        for date in self.full_date_operation:
+            while len(dates) != 5:
+                print(1)
+                dates.append(date)
+                pass
+            pass
+
+        #for operation in self.load_operation_json:  # цикл в джисон +
+            #if "date" in operation:
+                    #if date == operation["date"]:
+                        #print(operation["date"])
+                        #dates.append(operation)
+        #return dates
+
+
 
 a = account_transactions()
-b = "2019-08-26T10:50:58.294041"
-#print(a.correct_format(1))
 
 
-i = 0
+#print(a.correct_format())
 qwerty = []
-while i != len(a.correct_date()[1]):
-    #qwerty.append(a.correct_format(i))
-    qwerty = a.correct_format(i)
-    #print(i)
-    i += 1
-    print(f"{qwerty}")
-
-
-
-print(a.correct_date()[0])
+opera = 0
+while opera != 5:
+    #print(opera)
+    qwerty.append(a.correct_format())
+    opera += 1
+#print(qwerty)
+#for i in qwerty:
+#    print(i)
