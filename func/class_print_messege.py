03fdadae70ffc3c3eb878ba47a205ec8d7518dd4
YTKA <stasfyai73364@gmail.com>
2023-09-26 04:36:53 +0700

    пишем тестики

--- func/class_print_messege.py
+++ func/class_print_messege.py
@@ -13,8 +13,7 @@ def load_operation_json():
 class account_transactions:
     def __init__(self, operation_json):
         """
-        А что нужно прописывать в self?
-        :param operation_json: список операция
+        :param operation_json: список операций
         """
         self.date = []  # список последних 5 операция
         self.load_operation_json = operation_json  # получение операций
@@ -41,7 +40,6 @@ class account_transactions:
 
     def sort_operation(self):
         """
-
         :return: Список из дат последних 5 операций
         """
         self.full_date_operation = sorted(
@@ -50,6 +48,10 @@ class account_transactions:
 
     @staticmethod
     def correct_format_date(original_date):
+        """
+        :param original_date:  Получаем дату по типу 2022-01-01T12:00:00.000000
+        :return: Возвращаем дату по типу 01.01.2022
+        """
         datetime_obj = datetime.strptime(original_date, "%Y-%m-%dT%H:%M:%S.%f")
         correct_date = datetime_obj.strftime("%d.%m.%Y")
         return correct_date
@@ -77,22 +79,38 @@ class account_transactions:
         return self.date
 
     def from_card_hide(self, sender_number):
+        """
+        :param sender_number: Получчаем значение карты по типу Visa Classic 1234567890123456
+        :return: Возвращаем Visa Classic 1234 56** **** 3456
+        """
         string = ""
         for card_type, (start_index, end_index) in self.card_types.items():  # цикл в card_types
             if card_type in sender_number:  # если карта есть в списке
                 sender_number = sender_number[:0] + sender_number[start_index:]  # получаем только цифры
                 sender_number = sender_number[:-(len(sender_number) - 6)] + '*' * (len(sender_number) - 10) + sender_number[-4:]  # получаем значение по типу "124637******3588"
                 b = [sender_number[i:i + 4] for i in range(0, len(sender_number), 4)]  # получаем список из номера разделлных по 4
+
                 for el in b:
                     string += el + " "
+
                 return f"{card_type} {string}"
+
     def to_card_hide(self, sender_number):
+        """
+        :param sender_number: Принимаем значение по типу MasterCard 1234567890123456
+        :return: Возвращаем MasterCard **3456
+        """
         for card_type, values in self.card_types.items():
             if card_type in sender_number:
                 sender_number = "**" + sender_number[-4:]
+
                 return f"{card_type} {sender_number}"
 
     def print_message(self):
+        """
+        Функция для получение значений по операции и написания сообщения пользователю
+        :return:
+        """
         for operation in self.receiving_data():
             self.id_operation = operation["id"]  # id
             self.correct_date = self.correct_format_date(operation["date"])  #дата операции
@@ -105,6 +123,7 @@ class account_transactions:
                 self.from_operation = self.from_card_hide(operation["from"])  #откуда перевод
             else:
                 pass
+
             print(f"""{self.correct_date} {self.description_operation}
 {self.from_operation} -> {self.to_operation}
 {self.operation_amount} {self.name_operation}\n""")
