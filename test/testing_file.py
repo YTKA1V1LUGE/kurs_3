5432d7a5ffba855485aa8a6d3022b31be0a1e575
YTKA <stasfyai73364@gmail.com>
2023-09-27 21:50:48 +0700

    убираем класс и переделываем тесты

--- test/testing_file.py
+++ test/testing_file.py
@@ -30,42 +30,45 @@ operation_json = [{
             "description": "Перевод организации",
             "from": "MasterCard 7158300734726758",
             "to": "Счет 35383033474447895560"
-        }]
-
-account = class_print_messege.account_transactions(operation_json)
+        },
+]
 
 
 def test_correct_format_date():
     original_date = "2022-01-01T12:00:00.000000"
     expected_result = "01.01.2022"
-    assert account.correct_format_date(original_date) == expected_result
+    assert class_print_messege.correct_format_date(original_date) == expected_result
 
 
 def test_sort_operation():
     expected_result = ['2019-08-26T10:50:58.294041', "2019-07-03T18:35:29.512364"]
-    assert account.sort_operation() == expected_result
+    assert class_print_messege.sort_operation(operation_json) == expected_result
 
 
 def test_receiving_data():
-    expected_result = operation_json[0]
-    assert account.receiving_data()[0] == expected_result
+    expected = operation_json[0]
+    sort = class_print_messege.sort_operation(operation_json)
+    receiving_0 = class_print_messege.receiving_data(sort)[0]
+    assert receiving_0 == expected
 
 
 def test_from_card_hide():
     sender_number = "Visa Classic 1234567890123456"
     expected_result = "Visa Classic 1234 56** **** 3456 "
-    assert account.from_card_hide("") is None
-    assert account.from_card_hide(sender_number) == expected_result
+    assert class_print_messege.from_card_hide("") is None
+    assert class_print_messege.from_card_hide(sender_number) == expected_result
 
 
 def test_to_card_hide():
     sender_number = "MasterCard 1234567890123456"
     expected_result = "MasterCard **3456"
-    assert account.to_card_hide(sender_number) == expected_result
+    assert class_print_messege.to_card_hide(sender_number) == expected_result
 
 
 def test_print_message(capsys):
-    account.print_message()
+    sort = class_print_messege.sort_operation(operation_json)
+    receiving = class_print_messege.receiving_data(sort)
+    class_print_messege.print_message(receiving)
     captured = capsys.readouterr()
     expected_output = """26.08.2019 Перевод организации
 Maestro 1596 83** **** 5199  -> Счет **9589
